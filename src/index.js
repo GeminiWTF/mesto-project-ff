f6abf28397e63fe5bb9d4f9e642c24151c3dd86b
Olga <_bonny_@mail.ru>
2024-07-14 20:38:25 +0300

    like-button

--- src/index.js
+++ src/index.js
@@ -14,6 +14,14 @@ function createCard(item, cardDelete) {
     cardElement.querySelector('.card__title').textContent = item.name;
     cardElement.querySelector('.card__image').src = item.link;
     cardElement.querySelector('.card__image').alt = item.alt;
+
+    cardElement.querySelector('.card__like-button').addEventListener('click', function (evt) {
+        const eventTarget = evt.target;
+        evt.target.classList.toggle('card__like-button_is-active');
+      })
+    
+
+
  const deleteButton = cardElement.querySelector('.card__delete-button');
  deleteButton.addEventListener('click', cardDelete);
  return cardElement;
@@ -36,3 +44,21 @@ initialCards.forEach(function (element) {
     places.append(card);});
 
  
+
+
+
+
+// likeButton.forEach((button, index) => {
+//     button.onclick = () => toggleIsLiked(likeButton[index], button);
+//   });
+
+//   function toggleIsLiked(heart, button) {
+//     heart.classList.toggle('is-liked');
+//     setButtonText(heart, button);
+//   }
+
+// const addCard = document.querySelector('profile__add-button');
+// addCard.addEventListener('click', function (evt) {
+//     createCard(item, cardDelete);
+//   });
+
